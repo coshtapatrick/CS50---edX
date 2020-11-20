c13ab4738b4fa5d21148f13dda0f1a53d6a22d03
coshtapatrick <73592980+coshtapatrick@users.noreply.github.com>
2020-11-20 18:12:23 -0300

    Update mario_less.py

--- pset6/mario/mario_less.py
+++ pset6/mario/mario_less.py
@@ -1,1 +1,11 @@
 # Made by PMBC
+from cs50 import get_int
+
+h = -1
+
+while h<1 or h>8:
+    h = get_int("Height: ")
+for i in range(h):
+    n = i + 1
+    print(' ' * (h - n), end="")
+    print('#'* n)
