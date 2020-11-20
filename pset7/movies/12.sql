840e5324363e778e38d275519c7ea7191ecbf737
coshtapatrick <73592980+coshtapatrick@users.noreply.github.com>
2020-11-20 18:30:40 -0300

    Create 12.sql

---
+++ pset7/movies/12.sql
@@ -0,0 +1,11 @@
+SELECT title
+FROM movies
+WHERE id IN
+(SELECT stars.movie_id
+FROM stars JOIN people
+ON stars.person_id = people.id
+WHERE people.name = 'Johnny Depp' AND stars.movie_id IN
+(SELECT stars.movie_id
+FROM stars JOIN people
+ON stars.person_id = people.id
+WHERE people.name = 'Helena Bonham Carter'));
