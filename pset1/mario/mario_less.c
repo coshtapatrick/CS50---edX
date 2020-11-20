bb38e495668c6a452745ba8f43f6b1a2bbb4c04f
coshtapatrick <73592980+coshtapatrick@users.noreply.github.com>
2020-11-20 11:08:05 -0300

    Create mario_less.c

---
+++ pset1/mario/mario_less.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    while (true)
+    {
+        int hgt = get_int("Height:");
+        if (hgt>0 && hgt <9)
+        {
+            for(int i=1;i<=hgt;i++)
+            {
+                for(int j=i; j<hgt; j++){printf(" ");}
+                for(int x=0; x<i;x++){printf("#");}
+                printf("\n");
+            }
+            break;
+        }
+    }
+}
