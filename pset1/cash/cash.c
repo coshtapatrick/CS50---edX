3bf7734f3b9a5e9ed31e8f7c0603aaebedc2f73b
coshtapatrick <73592980+coshtapatrick@users.noreply.github.com>
2020-11-20 11:01:07 -0300

    Update cash.c

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,22 +1,37 @@
+// First problem of CS50 edX course
+// Also: my first-ever program in C
+// The porpouse of this is to count how many coins will be given to the client as change
+
 #include <stdio.h>
-#include <cs50.h>
+#include <cs50.h> // Special Library from the course cs50, responsible for the get_float() funciton
 
 int main(void)
 {
     while (true)
     {
-        int cents = 1000*get_float("Change owed:");
-        if (cents>0)
+        int cents = 1000 * get_float("Change owed:"); // takes the value in float that has to be 
+        if (cents > 0) // verifies if the inputed value is valid
         {
+            // the store only has coins of 25, 10, 5 and 1 cents
+            // creates a var that holds the total of coins
             int coins = 0;
-            coins += cents/250;
-            cents=cents%250;
-            coins += cents/100;
-            cents=cents%100;
-            coins += cents/50;
-            cents=cents%50;
-            coins += cents/10;
-            printf("%i\n",coins);
+
+            coins += cents / 250; // adds to 'coins' a int amount of "25 cents" coins
+            cents = cents % 250; // keeps whats left
+
+            // do the same for the "10 cents" coins
+            coins += cents / 100;
+            cents = cents % 100;
+
+            // repeat for the "5"
+            coins += cents / 50;
+            cents = cents % 50;
+
+            // whats is the change in 1's 
+            coins += cents / 10;
+
+            // prints the result: total count of coins
+            printf("%i\n", coins);
             break;
         }
     }
