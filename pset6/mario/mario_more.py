6582f2ec141f59f2b22446e0bdd3a4c8e74b60fd
coshtapatrick <73592980+coshtapatrick@users.noreply.github.com>
2020-11-20 18:13:22 -0300

    Create mario_more.py

---
+++ pset6/mario/mario_more.py
@@ -0,0 +1,10 @@
+# Made by PMBC
+from cs50 import get_int
+
+h = -1
+
+while h<1 or h>8:
+    h = get_int("Height: ")
+for i in range(h):
+    n = i + 1
+    print(' ' * (h - n),'#' * n,'  ','#' * n, sep = '')
